198cfe62ae0b37be90084746d48c155d5bb126f0
Gok <gokhan.altunbas@imerir.com>
2017-01-17 09:19:33 +0100

    008 app 13 choosingImages

--- app/src/main/java/fr/imerir/yora/activities/BaseActivity.java
+++ app/src/main/java/fr/imerir/yora/activities/BaseActivity.java
@@ -1,50 +1,100 @@
 package fr.imerir.yora.activities;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.View;
 
 import fr.imerir.yora.R;
 import fr.imerir.yora.infrastructure.YoraApplication;
 import fr.imerir.yora.views.NavDrawer;
 
-public abstract class BaseActivity extends AppCompatActivity{
+public abstract class BaseActivity extends AppCompatActivity {
 
     protected YoraApplication application;
     protected Toolbar toolbar;
     protected NavDrawer navDrawer;
+    protected boolean isTablet;
+
 
     @Override
-    protected void onCreate(Bundle savedState){
+    protected void onCreate(Bundle savedState) {
         super.onCreate(savedState);
 
         application = (YoraApplication) getApplication();
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        isTablet = (metrics.widthPixels / metrics.density) >= 600;
     }
 
     @Override
-    public void setContentView(@LayoutRes int layoutResId){
+    public void setContentView(@LayoutRes int layoutResId) {
 
         super.setContentView(layoutResId);
 
         toolbar = (Toolbar) findViewById(R.id.include_toolbar);
 
-        if(toolbar != null){
+        if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
     }
 
-    protected void setNavDrawer(NavDrawer drawer){
-        this.navDrawer = drawer;
-        this.navDrawer.create();
+    public void fadeOut(final FadeOutListener listener) {
+
+        View rootView = findViewById(android.R.id.content);
+        rootView.animate()
+                .alpha(0)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        listener.onFadeOutEnd();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                })
+                .setDuration(300)
+                .start();
+
     }
 
-    public Toolbar getToolbar(){
+    protected void setNavDrawer(NavDrawer drawer) {
+        this.navDrawer = drawer;
+        this.navDrawer.create();
+
+        overridePendingTransition(0, 0);     // ne pas utiliser les animations sur le systeme
+
+        View rootView = findViewById(android.R.id.content);
+        rootView.setAlpha(0);       //completement transparent
+        rootView.animate().alpha(1).setDuration(450).start();
+    }
+
+    public Toolbar getToolbar() {
         return toolbar;
     }
 
-    public YoraApplication getYoraApplication(){
+    public YoraApplication getYoraApplication() {
 
         return application;
     }
+    public interface FadeOutListener{
+
+        void onFadeOutEnd();
+    }
+
 }
--- app/src/main/java/fr/imerir/yora/activities/ProfileActivity.java
+++ app/src/main/java/fr/imerir/yora/activities/ProfileActivity.java
@@ -1,18 +1,27 @@
 package fr.imerir.yora.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import fr.imerir.yora.R;
 import fr.imerir.yora.views.MainNavDrawer;
 
-/**
- * Created by student on 16/01/2017.
- */
 
 public class ProfileActivity extends BaseAuthenticatedActivity{
     @Override
     protected void onYoraCreate(Bundle savedState) {
         setContentView(R.layout.activity_profile);
         setNavDrawer(new MainNavDrawer(this));
+
+        if(!isTablet){
+            View textFields = findViewById(R.id.activity_profile_textFields);
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) textFields.getLayoutParams();
+            params.setMargins(0,params.getMarginStart(),0,0);
+            params.removeRule(RelativeLayout.END_OF);
+            params.addRule(RelativeLayout.BELOW, R.id.activity_profile_changeAvatar);
+            textFields.setLayoutParams(params);
+        }
     }
 }
